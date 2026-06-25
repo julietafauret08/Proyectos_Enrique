a88a96de0cc56c4bf5f0328d4622acf3204e6b51
julietafauret08 <julietafauret08@gmail.com>
2026-06-25 11:15:57 -0300

    cambios de carpetas

--- TpCasino/MAIN.js
+++ TpCasino/MAIN.js
@@ -1,100 +1,29 @@
-// Clase base
-class Heroe {
-    constructor(nombre, vida, mascota, vestimenta, herramienta, resistencia) {
-        this.nombre = nombre;
-        this.vida = vida;
-        this.mascota = mascota;
-        this.vestimenta = vestimenta;
-        this.herramienta = herramienta;
-        this.resistencia = resistencia;
-    }
+const Casino = require("./Casino");
+const BlackJack = require("./BlackJack");
+const Ruleta = require("./Ruleta");
+const Tragamonedas = require("./Tragamonedas");
+const Camaras = require("./Camaras");
 
-    comer() {
-        this.vida += 10;
-        console.log(`${this.nombre} comió y recuperó vida. Vida actual: ${this.vida}`);
-    }
+// Crear casino
+const casino = new Casino("Casino Central", 500, 5);
 
-    descansar() {
-        this.resistencia += 5;
-        console.log(`${this.nombre} descansó. Resistencia actual: ${this.resistencia}`);
-    }
-}
+// Crear juegos
+const blackjack = new BlackJack("BlackJack", "VIP", 10, 5, 100);
+const ruleta = new Ruleta("Ruleta", "Clásica", 8, 1000);
+const tragamonedas = new Tragamonedas("Tragamonedas", "Electrónica", 20, "Sony", "Digital", 5000);
 
-// Clase Mago
-class Mago extends Heroe {
-    constructor(nombre, vida, mascota, vestimenta, herramienta, resistencia, mana, poderMagico) {
-        super(nombre, vida, mascota, vestimenta, herramienta, resistencia);
-        this.mana = mana;
-        this.poderMagico = poderMagico;
-    }
+// Agregar juegos al casino
+casino.agregarJuego(blackjack);
+casino.agregarJuego(ruleta);
+casino.agregarJuego(tragamonedas);
 
-    lanzarHechizo(enemigo) {
-        if (this.mana >= 10) {
-            enemigo.vida -= this.poderMagico;
-            this.mana -= 10;
-            console.log(`${this.nombre} lanzó un hechizo a ${enemigo.nombre}.`);
-            console.log(`Vida de ${enemigo.nombre}: ${enemigo.vida}`);
-        } else {
-            console.log(`${this.nombre} no tiene suficiente maná.`);
-        }
-    }
+// Crear cámara
+const camara = new Camaras("Canon", "HD");
 
-    recuperarMana() {
-        this.mana += 20;
-        console.log(`${this.nombre} recuperó maná. Maná actual: ${this.mana}`);
-    }
-}
-
-// Clase Caballero
-class Caballero extends Heroe {
-    constructor(nombre, vida, mascota, vestimenta, herramienta, resistencia, fuerza) {
-        super(nombre, vida, mascota, vestimenta, herramienta, resistencia);
-        this.fuerza = fuerza;
-    }
-
-    atacar(enemigo) {
-        enemigo.vida -= this.fuerza;
-        console.log(`${this.nombre} atacó a ${enemigo.nombre}.`);
-        console.log(`Vida de ${enemigo.nombre}: ${enemigo.vida}`);
-    }
-
-    defender() {
-        this.resistencia += 10;
-        console.log(`${this.nombre} se defendió. Resistencia: ${this.resistencia}`);
-    }
-}
-
-// Crear personajes
-const mago = new Mago(
-    "Merlín",
-    100,
-    "Búho",
-    "Túnica Azul",
-    "Bastón",
-    30,
-    50,
-    25
-);
-
-const caballero = new Caballero(
-    "Arturo",
-    120,
-    "Caballo",
-    "Armadura",
-    "Espada",
-    40,
-    20
-);
-
-// Simulación del juego
-console.log("=== COMBATE ===");
-
-caballero.atacar(mago);
-mago.lanzarHechizo(caballero);
-caballero.defender();
-mago.recuperarMana();
-mago.comer();
-
-console.log("\n=== ESTADO FINAL ===");
-console.log(mago);
-console.log(caballero);
+// Mostrar info
+console.log(casino.info());
+console.log(blackjack.info());
+console.log(ruleta.info());
+console.log(tragamonedas.info());
+console.log(camara.usarCamara("Mesa VIP"));
+console.log(camara.observarSeccion("Sección Clásica"));
